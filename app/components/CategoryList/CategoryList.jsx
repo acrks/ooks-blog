d261d4ace78cc67ecdb13a59374af93c4e386c76
Alex Crooks <alexcrooks2@gmail.com>
2024-04-11 13:13:38 -0700

    Added category pill and dynamic background colors

--- app/components/CategoryList/CategoryList.jsx
+++ app/components/CategoryList/CategoryList.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
+import { getCatThemeColor } from '@/app/api/functions'
 
 
 const CategoryList = ({categories}) => {
@@ -13,7 +14,7 @@ const CategoryList = ({categories}) => {
         <h1 className='text-[30px]'><b>Popular Categories</b></h1>
         <div className='flex justify-evenly mt-[50px] gap-x-1'>
             {categories?.map((item) => (
-                <Link className="bg-[#16131a] rounded-lg drop-shadow-[0px_0px_5px_rgba(253,224,71,0.25)] hover:scale-[1.025] hover:drop-shadow-[0_0_7px_rgba(253,224,71,0.5)] transition-all duration-200" href={`/blogs/${item.slug}`} key={item.id}>
+                <Link className={`${getCatThemeColor('bg', item.slug)} rounded-lg drop-shadow-[0px_0px_5px_rgba(253,224,71,0.25)] hover:scale-[1.025] hover:drop-shadow-[0_0_7px_rgba(253,224,71,0.5)] transition-all duration-200`} href={`/blogs/${item.slug}`} key={item.id}>
                 <div className='flex items-center justify-center w-[350px] h-[100px] rounded-lg p-1 gap-x-3'>
                 <Image src={item.img} alt="" className='h-10 w-10 rounded-full object-cover -z-49' width={36} height={36} />                    
                 <p>{item.title}</p>
